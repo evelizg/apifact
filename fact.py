43da9a2546b5bd0d2a8a9d93dcf31695ef9c313e
Erwin <erwinveliz@credicorpcapital.com>
2021-07-07 10:03:17 -0500

    factAPI

--- fact.py
+++ fact.py
@@ -1,13 +1,9 @@
-from PIL import Image
-#img  = Image.open(path)     
-# On successful execution of this statement,
-# an object of Image type is returned and stored in img variable)
-   
-try: 
-    img  = Image.open("/mnt/c/Users/EVELIZGR/OneDrive/Nego/api/factura-autonomo.jpg")
-except IOError:
-    pass
-img.save("/mnt/c/Users/EVELIZGR/OneDrive/Nego/api/out/factura-autonomo01.jpg")
-# Use the above statement within try block, as it can 
-# raise an IOError if file cannot be found, 
-# or image cannot be opened.
+from flask import Flask, request, jsonify
+import requests
+
+url = 'http://127.0.0.1:5000/im_size'
+my_img = {'image': open('/mnt/c/Users/EVELIZGR/OneDrive/Nego/api/factura-autonomo.jpg', 'rb')}
+r = requests.post(url, files=my_img)
+
+# convert server response into JSON format.
+print(r.json())
